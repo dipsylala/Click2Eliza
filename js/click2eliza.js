f15552b0fa57066cea9ca18d30046ca55015c3f9
mwatson@veracode.com <mwatson@veracode.com>
2017-01-26 11:21:25 +0000

    Initial commit

---
+++ js/click2eliza.js
@@ -0,0 +1,108 @@
+"use strict";
+
+var Click2Eliza = (function(){
+
+	var _eliza;
+	var _initialiseFunction;
+	var _addButtonFunction;
+	var _pageCheckerInterval;
+
+	var _elizaResponse = function(userInput){
+
+		if (userInput === '' || userInput === undefined){
+			return;
+		}
+		var chatResponse = $("#chat-response");
+
+        chatResponse.append("YOU: " + userInput + "\n");
+        chatResponse.append("ASC: " + _eliza.transform(userInput) + "\n");
+	};
+
+	var _startElizaChat = function(){
+		_eliza = new ElizaBot();
+		var initial = _eliza.getInitial();
+		$("#chat-response").append(initial + "\n");
+	};
+
+	var _openElizaDialog = function(){
+		$( "#chat-dialog" ).dialog( "open" );
+	};
+
+	var initialiseChatDialog = function(){
+
+		$.get(chrome.extension.getURL('html/chat.html'), function(data) {
+			// $($.parseHTML(data)).appendTo('body')
+		    $(data).appendTo('body');
+
+			$("#chat-dialog").dialog({
+				resizable: false,
+				autoOpen: false,
+				width: 310,
+				height: 320
+			});
+
+			var chatText = $('#chat-text');
+
+            chatText.keypress(function (e) {
+  				if (e.which == 13) {
+    				$('#chat-form').submit();
+    				return false;
+  				}
+			});
+
+			$("#chat-form").submit(function(event){
+				var chatText = chatText[0].value;
+				_elizaResponse(chatText);
+                chatText[0].value = '';
+  				event.preventDefault();
+			});
+
+
+			_addButtonFunction();
+		});
+	};
+
+	var addButtonToPlatformResults = function (){
+
+        var click2chatBtn = $("#click2chat-btn");
+
+        click2chatBtn.appendTo("#resultSummaryButtonContainer");
+        click2chatBtn.show();
+        click2chatBtn.click(function(){
+			_openElizaDialog();
+			_startElizaChat();
+		});		
+	};
+
+	var checkValidPage = function(){
+		if (_initialiseFunction === undefined || document.title.indexOf("Veracode") === -1){
+			return;
+		}
+
+		if (document.getElementById ("resultSummaryButtonContainer")){
+			clearInterval (_pageCheckerInterval);
+			_initialiseFunction();
+		}
+	};
+
+	var startPageChecker = function(initialiseFunctionCallback, addButtonFunctionCallback){
+		// functions as parameters for injection (unit testing)
+		_initialiseFunction = initialiseFunctionCallback;
+		_addButtonFunction = addButtonFunctionCallback;
+
+		// We have to use interval checking due to async page loading.
+		// Can't rely on normal DOM ready functions		
+		_pageCheckerInterval = setInterval (checkValidPage, 111);
+	};
+
+	return {
+		checkValidPage:checkValidPage,
+		initialiseChatDialog:initialiseChatDialog,
+		addButtonToPlatformResults:addButtonToPlatformResults,
+		startPageChecker:startPageChecker
+	}
+}());
+
+$(function(){
+	Click2Eliza.startPageChecker(Click2Eliza.initialiseChatDialog, Click2Eliza.addButtonToPlatformResults);
+});
